e0a07a7172fee61b195a026cd543ccc407498c92
HyunWoo <zzanghw91@naver.com>
2023-03-06 00:30:55 +0900

    코딩 완료

--- scripts/config.js
+++ scripts/config.js
@@ -1,28 +1,36 @@
 function openPlayerConfig(event) {
-    let editedPlayer = +event.target.dataset.playerid; //+'1' => 1
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
-};
+  editedPlayer = +event.target.dataset.playerid; //+'1' => 1
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
+}
 
 function closePlayerConfig() {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
-    formElement.firstElementChild.classList.remove('error');
-    errorsOutputElement.textContent = '';
-};
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove("error");
+  errorsOutputElement.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = ""; //좋은방법아니지만 예시로 사용해봄
+}
 
 function savePlayerConfig(event) {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const enteredPlayername = formData.get('playername').trim();    //trim은 앞뒤 공백 삭제
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim(); //trim은 앞뒤 공백 삭제
 
-    if (!enteredPlayername) {    //enteredPlayername === ''
-        event.target.firstElementChild.classList.add('error');
-        errorsOutputElement.textContent = '*이름을 입력해주세요.';
-        return;
-    }
+  if (!enteredPlayername) {
+    //enteredPlayername === ''
+    event.target.firstElementChild.classList.add("error");
+    errorsOutputElement.textContent = "*이름을 입력해주세요.";
+    return;
+  }
 
-    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
-    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+  const updatedPlayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
 
-};
+  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+
+  players[editedPlayer - 1].name = enteredPlayername;
+
+  closePlayerConfig();
+}
